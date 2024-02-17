025c790640b0b99e1f11c711a9559a68fc276a9f
lvy <upwardsrwr@gmail.com>
2024-02-17 23:33:59 +0800

    feat: zset

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -9,3 +9,11 @@ export function generateRandomString(length: number): string {
 
   return result
 }
+
+//封装随机函数
+// export function generateRandomNumber(length: number): number {
+//   const characters = '0123456789'
+//   const result = ''
+//   const charactersLength = characters.length
+//   for (let i = 0; i < length; i++) {}
+// }
