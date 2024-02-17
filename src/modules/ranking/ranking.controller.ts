8e06341bd34ca3b48d64f82c54685ed2fe077d13
lvy <upwardsrwr@gmail.com>
2024-02-17 23:57:03 +0800

    feat: zset

--- src/modules/ranking/ranking.controller.ts
+++ src/modules/ranking/ranking.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Inject, Query } from '@nestjs/common'
+import { RankingService } from './ranking.service'
 
 @Controller('ranking')
-export class RankingController {}
+export class RankingController {
+  @Inject(RankingService)
+  rankingService: RankingService
+
+  @Get('join')
+  async join(@Query('name') name: string) {
+    await this.rankingService.join(name)
+    return 'success'
+  }
+
+  @Get('learn')
+  async addLearnTime(@Query('name') name: string, @Query('time') time: string) {
+    await this.rankingService.addLearnTime(name, parseFloat(time))
+    return 'success'
+  }
+
+  @Get('monthRanking')
+  async getMonthRanking() {
+    return this.rankingService.getMonthRanking()
+  }
+
+  @Get('yearRanking')
+  async getYearRanking() {
+    return this.rankingService.getYearRanking()
+  }
+}
